d20bc9effc91862860ecbda4f7d20282d681a947
Deborah Thompson <deb246@btinternet.com>
2018-12-24 19:56:08 +0000

    base template added

--- run.py
+++ run.py
@@ -1,14 +1,35 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
+
+
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
+    
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+
+
+@app.route('/feedback')
+def feedback():
+    return render_template("feedback.html")
+
+
+@app.route('/newquestions')
+def newquestions():
+    return render_template("newquestions.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
